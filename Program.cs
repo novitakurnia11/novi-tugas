3ec414cc6807a81e8f464fbe0efd7da0971fe6c3
ASUS <ASUS@DESKTOP-ADSL7MD>
2023-04-03 21:23:41 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxiifinal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            taxigas taxi = new taxigas();
+
+            Console.Write("Masukan Nama Driver : ");
+            taxi.DriveName = Console.ReadLine();
+
+            Console.Write("Masukan Jumlah Penumpang : ");
+            taxi.NumPassenger = Convert.ToInt16(Console.ReadLine());
+
+            if (taxi.NumPassenger > 0)
+            {
+                taxi.info();
+                taxi.up();
+                taxi.down(); ;
+
+                Console.ReadLine();
+            }
+            else
+                taxi.info();
+            taxi.ngangur();
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
---
+++ taxi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace taxiifinal
+{
+    internal class taxigas
+    {
+
+        public string DriveName { get; set; }
+        public Boolean OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void info()
+        {
+            Console.WriteLine("\n Taxi Info \n");
+            Console.WriteLine("Nama Driver : {0}", DriveName);
+
+            if (NumPassenger > 0)
+            {
+                Console.WriteLine("Kerja : Iya ", OnDuty);
+            }
+            else
+            {
+
+                Console.WriteLine("Kerja : Tidak", OnDuty);
+            }
+            Console.WriteLine("Jumlah Penumpang : {0}", NumPassenger);
+
+        }
+        public void ngangur()
+        {
+            Console.WriteLine("\n \n TIDAK BEKERJA \n \n", false);
+        }
+        public void up()
+        {
+            Console.WriteLine("\n {0} Sedang Menaikkan Penumpang", DriveName);
+        }
+        public void down()
+        {
+            Console.WriteLine("\n {0} Sedang Menurunkan Penumpang", DriveName);
+        }
+    }
+}
